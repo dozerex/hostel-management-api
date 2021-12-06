c5ea02a88f142d1282293e389688446f5b09e22b
Yuvarajan G <yuvigopi@outlook.com>
2021-12-06 13:06:10 +0530

    Setup Express

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+    res.send("<h1>This is IIITL - Hostel Management API</h1>");
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port '+ port);
+})
