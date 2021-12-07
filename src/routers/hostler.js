941a1f626a2ee7b71fe96e508f49a7f9359fd085
Yuvarajan G <yuvigopi@outlook.com>
2021-12-07 10:19:51 +0530

    Add Logout router

--- src/routers/hostler.js
+++ src/routers/hostler.js
@@ -23,4 +23,26 @@ router.get('/me', auth, async (req, res) => {
     res.send(req.user);
 })
 
+router.post('/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save();
+        res.send({name: req.user.name, message: "Logout Success!"});
+    } catch(e) {
+        res.status(500).send();
+    }
+})
+
+router.post('/logoutall', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send("Logout Successfully from all devices", req.user.name);
+    } catch(e) {
+        res.status(500).send();
+    }
+})
+
 module.exports = router;
