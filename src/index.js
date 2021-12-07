2a1f4e8b7ec2b4a506e4002e2ac1c2aa1b9f6b53
Yuvarajan G <yuvigopi@outlook.com>
2021-12-07 10:40:57 +0530

    Replace with env

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const admin = require('./routers/admin');
 const hostler = require('./routers/hostler');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
