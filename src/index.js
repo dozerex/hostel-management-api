029215a7f2f6377ab5373a90fd74a972011885f0
Yuvarajan G <yuvigopi@outlook.com>
2021-12-09 18:51:16 +0530

    Cookie

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('./db/mongoose');
-// const cors = require('cors')
+const cors = require('cors')
 
 const cookieParser = require('cookie-parser');
 
@@ -13,9 +13,9 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({
-//     origin: 'https://127.0.0.1:3000'
-// }))
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.use(admin);
 app.use(hostler);
--- src/routers/hostler.js
+++ src/routers/hostler.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Hostler = require('../models/hostler');
 
-
 const auth = require('../middleware/hostlerAuth');
 
 const router = new express.Router();
@@ -11,9 +10,28 @@ router.post('/', async (req, res) => {
 })
 
 
+// router.post('/islogined', async (req, res) => {
+//     try {
+//         console.log("Hello")
+//         const token = req.body.token;
+//         console.log(token);
+//         const decoded = jwt.verify(token, process.env.JWT_KEY);
+//         const user = await Hostler.findOne({ _id: decoded._id, 'tokens.token': token });
+//         if(!user) {
+//             throw new Error();
+//         }
+//         console.log(user);
+//         res.send("hello");
+//     } catch(e) {
+//         // console.log("uesr", user)
+//         res.send("how");
+//     }
+// })
+
+
 router.use('/login', async (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        const token = req.body.token;
         console.log(token);
         // const token = req.header('Authorization').replace('Bearer ','');
         const decoded = jwt.verify(token, process.env.JWT_KEY);
@@ -29,17 +47,18 @@ router.use('/login', async (req, res, next) => {
     }
 })
 
+
 router.post('/login', async (req, res) => {
     try {
         const user = await Hostler.findByCredentials(req.body.email, req.body.password);
         console.log("kajsdofh iaodsfo")
         const token = await user.generateAuthToken();
-        res.cookie("token", token, {
-            // httpOnly: true,
-            expires: new Date(Date.now()+240000),
-            // secure: true    
-        })
-        res.send(user);
+        // res.cookie("token", token, {
+        //     // httpOnly: true,
+        //     expires: new Date(Date.now()+60000),
+        //     // secure: true    
+        // })
+        res.send({user, token});
     } catch(e) {
         res.status(400).send(e);
     }
