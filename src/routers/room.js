f17786461dd16bbeadd0d193951aa45fda93e3a1
Yuvarajan G <yuvigopi@outlook.com>
2021-12-10 19:59:37 +0530

    Roomchange route

--- src/routers/room.js
+++ src/routers/room.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Room = require('../models/room');
+const User = require('../models/hostler');
 
 const router = new express.Router();
 
@@ -16,9 +17,37 @@ router.post('/admin/addRoom', async(req, res) => {
     }
 });
 
-router.post('/admin/roomChange', async(req, res) => {
-    
+router.post('/admin/changeRoom', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const user = await User.findOne({ email });
+        const roomNumber = Number(req.body.room);
+        console.log(roomNumber);
+        const room = await Room.findOne({
+            roomNumber,
+            rem: {$gt: 0}
+        })
+        user.room = room._id;
+        console.log(user.room)
+        await user.save();
+        room.occupied-=1;
+        room.save();
+        res.send(user);
+    } catch(e) {
+        res.status(400).send();
+    }
 })
+
+// router.post('/admin/changeRoom/:roomNumber', async (req, res) => {
+//     try {
+//         const roomNumber = Number(req.params.roomNumber)
+//         const room = await Room.findOne({roomNumber});
+//         await room.populate({path: 'hostlers'});
+//         res.send(room.hostlers);
+//     } catch(e) {
+//         res.status(400).send(e);
+//     }
+// })
 
 
 module.exports = router;
