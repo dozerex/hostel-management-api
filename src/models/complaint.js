7a063cebce0034323984e11c8fc6b4a969a21370
Yuvarajan G <yuvigopi@outlook.com>
2021-12-10 12:02:05 +0530

    Complaint model

---
+++ src/models/complaint.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const complaintSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 10
+    },
+    location: {
+        type: String,
+        required: true,
+        maxlength: 15
+    },
+    status: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value<0 || value>2 ) {
+                throw new Error('Must be between 0 and 2');
+            }
+        }
+    },
+    image: {
+        type: Buffer
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+mongoose.model('Complaint', complaintSchema);
